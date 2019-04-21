a238858aeb57d31beebf4ceb9bfa0ccf7e2cebeb
dkozlova <47688518+dkozlova@users.noreply.github.com>
2019-04-21 18:02:00 +0300

    Update server.py

--- server.py
+++ server.py
@@ -3,15 +3,9 @@ from datetime import datetime as dt
 from random import random
 from horoscope import generate_prophecies
 
-@route('/<filename>')
-def server_static(filename):
-    return static_file(filename, root='\Desktop\Python\курсы\module5server')
-
 @route("/")
 
 @view("predictions")
-
-
 def index():
 	now = dt.now()
 	x = random()
@@ -26,13 +20,8 @@ def index():
 def api_test():
    return {"test": True}
    
-def styles_css():
-    return static_file("styles.css", root="..\styles.css")
-
 @route("/api/forecast")
 def forecast():
 	return ({"predictions": generate_prophecies(),})
 	
-
-
 run(host="localhost", port=8080, debug=True)
